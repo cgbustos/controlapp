925072665bc82b1358e4d7d7cb9c5286988dd4e7
Angel David Castro <davangcas@gmail.com>
2024-05-06 19:41:56 -0300

    test: condicionales

--- javascript.js
+++ javascript.js
@@ -38,14 +38,59 @@ var variable_de_alcance_global = 57;
 // tipos de variables segun el alcance al declarlas
 
 
-{
-    let variable_de_alcance_local = 56;
-    const variable_constante_de_alcance_local = 55;
-    console.log(variable_de_alcance_local)
+// {
+//     let variable_de_alcance_local = 56;
+//     const variable_constante_de_alcance_local = 55;
+//     console.log(variable_de_alcance_local)
+// }
+
+// console.log(variable_de_alcance_global)
+// console.log(variable_de_alcance_local)
+// console.log(variable_constante_de_alcance_local)
+
+
+// let variable_que_cambia_de_valor;
+// const constante = "";
+// var variable_antiguita;
+
+// primitivos
+let numero = 10
+let flotante = 10.12;
+let string = "cadena de caracteres"
+let booleano = true;
+let booleano2 = false;
+
+// compuestos
+const lista_uno = [numero, flotante, booleano, string];
+const objetos = {
+    "edad": numero,
+    "edad2": 56,
 }
 
-console.log(variable_de_alcance_global)
-console.log(variable_de_alcance_local)
-console.log(variable_constante_de_alcance_local)
+
+// logica
+/*
+estado logico alto o 1 logico o verdadero
+estado logico bajo o 0 logico o falso
+
+falsy
+    - false;
+    - cadena vacia
+    - 0
+truthy
+    - true;
+    - cadena cualquiera
+    - objecto o una variable compuesta
+    - cualquier numero distinto de 0
+*/
+
+let edad_pedrito = 5;
+let edad_juan = 10;
+
+if (edad_pedrito > edad_juan){
+    console.log("Pedrito esta viejo")
+} else {
+    console.log("Juan esta viejo")
+}
 
 
