af607988715eb62f2f9f46580622f580bd2ec11b
Angel David Castro <davangcas@gmail.com>
2024-05-13 19:00:47 -0300

    descargar videos o musica de youtube

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hola jose")
+from pytube import YouTube
+
+video_url = input("Ingrese la url del video: ")
+only_audio = input("¿Desea descargar solo el audio? (y/n): ")
+only_audio = only_audio == 'y'
+youtube_instance = YouTube(video_url)
+
+if only_audio:
+    audio_stream = youtube_instance.streams.filter(only_audio=True, mime_type="audio/mp4").order_by('abr').desc().first()
+    audio_stream.download(output_path='./music')
+else:
+    video_stream = youtube_instance.streams.filter(only_video=True).order_by('resolution').desc().first()
+    video_stream.download(output_path='./videos')
+
+print("Descarga completada.")
