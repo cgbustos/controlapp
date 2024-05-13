47804eb34aa91af03e4ce6f3d5b02bb142269107
Angel David Castro <davangcas@gmail.com>
2024-05-13 18:33:06 -0300

    html

--- javascript.js
+++ javascript.js
@@ -1,71 +1,71 @@
 
-var variable_de_alcance_global = 57;
+// var variable_de_alcance_global = 57;
 
-// tipos de variables segun dato a guardar
-
-
-// primitavas
-    // numerica
-    edad = 55;
-    console.log(edad);
-
-    // string o caracteres
-    nombre = "Juan ramon"
-    console.log(nombre)
-
-    // null
-    variable_null = null;
-    console.log(variable_null)
-
-    // undefined o indefinida
-    // joasgjokgak;
-    // console.log(joasgjokgak);
-    // NaN
-
-// compuestas
-    // objetos
-    objeto = {
-        nombre: "Juan ramon",
-        edad: 15,
-    }
-    console.log(objeto)
-    // arreglos
-    arreglo = [edad, nombre, objeto, 656]
-    //          0      1       2      3
-    console.log(arreglo[0])
+// // tipos de variables segun dato a guardar
 
 
-// tipos de variables segun el alcance al declarlas
+// // primitavas
+//     // numerica
+//     edad = 55;
+//     console.log(edad);
+
+//     // string o caracteres
+//     nombre = "Juan ramon"
+//     console.log(nombre)
+
+//     // null
+//     variable_null = null;
+//     console.log(variable_null)
+
+//     // undefined o indefinida
+//     // joasgjokgak;
+//     // console.log(joasgjokgak);
+//     // NaN
+
+// // compuestas
+//     // objetos
+//     objeto = {
+//         nombre: "Juan ramon",
+//         edad: 15,
+//     }
+//     console.log(objeto)
+//     // arreglos
+//     arreglo = [edad, nombre, objeto, 656]
+//     //          0      1       2      3
+//     console.log(arreglo[0])
 
 
-// {
-//     let variable_de_alcance_local = 56;
-//     const variable_constante_de_alcance_local = 55;
-//     console.log(variable_de_alcance_local)
+// // tipos de variables segun el alcance al declarlas
+
+
+// // {
+// //     let variable_de_alcance_local = 56;
+// //     const variable_constante_de_alcance_local = 55;
+// //     console.log(variable_de_alcance_local)
+// // }
+
+// // console.log(variable_de_alcance_global)
+// // console.log(variable_de_alcance_local)
+// // console.log(variable_constante_de_alcance_local)
+
+
+// // let variable_que_cambia_de_valor;
+// // const constante = "";
+// // var variable_antiguita;
+
+// // primitivos
+// let numero = 10
+// let flotante = 10.12;
+// let string = "cadena de caracteres"
+// let booleano = true;
+// let booleano2 = false;
+
+// // compuestos
+// const lista_uno = [numero, flotante, booleano, string];
+// const objetos = {
+//     "edad": numero,
+//     "edad2": 56,
 // }
-
-// console.log(variable_de_alcance_global)
-// console.log(variable_de_alcance_local)
-// console.log(variable_constante_de_alcance_local)
-
-
-// let variable_que_cambia_de_valor;
-// const constante = "";
-// var variable_antiguita;
-
-// primitivos
-let numero = 10
-let flotante = 10.12;
-let string = "cadena de caracteres"
-let booleano = true;
-let booleano2 = false;
-
-// compuestos
-const lista_uno = [numero, flotante, booleano, string];
-const objetos = {
-    "edad": numero,
-    "edad2": 56,
-}
 
 
 // logica
@@ -85,12 +85,47 @@ truthy
 */
 
 let edad_pedrito = 5;
-let edad_juan = 10;
+let edad_juan = "5";
+let comparador = edad_juan > edad_pedrito;
 
-if (edad_pedrito > edad_juan){
+if (comparador){
     console.log("Pedrito esta viejo")
 } else {
     console.log("Juan esta viejo")
 }
 
+// operadores de compracion
+/*
+> (mayor)
+>= (mayor o igual)
+< (menor o igual)
+<= (menor o igual)
+== (igual)
+
+propios de javascript
+=== (igual en valor y en tipo de dato) 3 === "3"
+<== (menor o igual en tipo de dato)
+>== (mayor o igual en tipo de dato)
+
+compracion logica
+&& false && true -> false
+|| false || true -> true
+
+*/
+
+if (edad_juan === edad_pedrito){
+    console.log("Hola jose")
+}
+
+
+// strings documentation - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// template_string
+let cadena_hola_largo = `
+<div>
+    <ul>
+        <li>Hola josema</li>
+    </ul>
+</div>
+`
+console.log(cadena_hola_largo)
 
